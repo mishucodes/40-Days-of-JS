a638c1b9ebaee5847943a98255dd2b734bf8d441
Manish Goyal <manishgoyal5683@gmail.com>
2025-11-23 00:06:16 +0530

    day 29

---
+++ Module 05 - OOP/Day 29 - Introduction to OOP/Basics of OOP.js
@@ -0,0 +1,16 @@
+//What are Objects: As far as OOP is concerned, we say that anything that has some characteristics &/or behavior(s), we can translate/represent that thing into/using an object. For instance:
+    //a) A Car is a Thing [Noun] with some:
+        //aa) Characteristics [Adjectives]: color, model, weight, price, brand.
+        //ab) Behaviours [Verbs]: start(), brake(), accelerate(), honk().
+    //NOTES:
+        //'Characteristics' are called 'Properties', 'Attributes', 'States', etc. in the programming world.
+        //'Behaviours' are called 'Methods', 'Actions', etc.
+
+
+
+//05) Five OOP Principles:
+    //a) Abstraction: Hiding the internal complexities of a system, & exposing only what is necessary part to the user. At the end of the day, user should just need to learn how to use a system, & not how it works.
+    //b) Encapsulation: Making sure that the user cannot temper with the sensitive data inside. It's not about user-convenience, it's about bad-faith actors.
+    //c) Inheritance: Objects inheriting certain properties/methods from other objects to avoid code duplication. Such inherited objects are of a "x is a y" structure. For instance: "this car is a vehicle".
+    //d) Polymorphism: The ability of different objects (often related by inheritance) to respond to the same operation or method call in their own way.
+    //e) Composition: Building complex objects by combining (“composing”) them from simpler, smaller objects. Such composed objects are of a "x has a/an y" structure. For instance: "this car (is a vehicle) that has a special sound system"
